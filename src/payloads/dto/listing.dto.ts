6794165415f4802138f6912fd05b37f7710b1fd6
hasilon88 <hasilon88@gmail.com>
2024-12-01 04:31:22 -0500

    Added RealtorApi to access data from the RealtorApi

---
+++ src/payloads/dto/listing.dto.ts
@@ -0,0 +1,37 @@
+interface Price {
+    USD?: number;
+    CAD?: number;
+    EUR?: number;
+}
+
+interface ListingBase {
+    id?: string;
+    address?: string;
+    city?: string;
+    state?: string;
+    zip_code?: string;
+    property_type?: string;
+    bedrooms?: number;
+    bathrooms?: number;
+    building_size?: number;
+    prices?: Price;
+    url?: string;
+}
+
+export interface ListingDetailed extends ListingBase {
+    land_size?: number;
+    description?: string;
+    images?: string[];
+    contact?: {
+        first_name?: string;
+        last_name?: string;
+        phone_number?: string;
+        profile_pic?: string | null;
+        company?: string;
+        company_url?: string;
+    };
+}
+
+export interface ListingOverview extends ListingBase {
+    image?: string;
+}
